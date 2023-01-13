f4d0e1545259fca7c5d75aefbcfa3ab36a5d8a32
Shubham3455 <88342839+Shubham3455@users.noreply.github.com>
2023-01-14 03:47:23 +0530

    Deployed Website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
+
 import './App.css';
+import "font-awesome/css/font-awesome.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='card'>
+    <div className='outer-box'>
+      <div className='box'>
+            <div className='element1'>
+
+            </div>
+            <div className='element2'>
+
+            </div>
+            <div className='element3'>
+
+            </div>
+           
+               
+
+      </div>
+      <div className='text'>
+          It’s a delight to have you onboard
+      </div>
+        <div className='text1'>
+        Help us know you better. (This is how we optimize Wobot as per your business needs) 
+        </div>
+        <div className='text1'>
+        Help us know you better. (This is how we optimize Wobot as per your business needs) 
+        </div>
+        <div className='Company-name'>
+        Company name 
+        
+        </div>
+        <div >
+        <input className='input1'></input>
+        </div>
+        <div className='Industry'>
+        Industry 
+        
+        </div>
+        <div >
+        <input className='input2'></input>
+        </div>
+        <div className='Company-size'>
+        Company size
+        
+        </div>
+        <div className='tag1'>
+        1-20
+        
+        </div>
+        <div className='tag2'>
+        21-50
+        
+        </div>
+        <div className='tag3'>
+        51-200
+        
+        </div>
+        <div className='tag4'>
+        201-500
+        
+        </div>
+        <div className='tag5'>
+        500+
+        
+        </div>
+        <div className='Get-Started'>
+        Get Started
+        
+        </div>
     </div>
+    </div>
+    </div>
+  
   );
 }
 
